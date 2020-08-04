971c51ebb7b84c40c37a3724a5756dc689ab8837
Rafi Hanif <git@github.com:rafihanif86>
2020-08-04 23:22:55 +0700

    membuat controler register

---
+++ database/migrations/2020_08_04_151431_create_tarifs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTarifsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarifs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('daya_listrik');
+            $table->text('tarif_listrik');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarifs');
+    }
+}
